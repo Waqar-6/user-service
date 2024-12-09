279ec4750eec29e2f51f2faf6508527cfc3e9ab0
Waqar <waqar-1@hotmail.co.uk>
2024-12-09 16:57:31 +0000

    chore: set up user service project using spring initializr

---
+++ src/test/java/com/w_farooq_group/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w_farooq_group.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
